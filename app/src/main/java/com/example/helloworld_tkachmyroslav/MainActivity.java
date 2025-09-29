c188b1ee976687c33cadc3cf4e545fa102dd9fa6
myrik <slava.1.4.tk1@gmail.com>
2025-09-29 14:55:10 +0300

    1 button added + text change function

--- app/src/main/java/com/example/helloworld_tkachmyroslav/MainActivity.java
+++ app/src/main/java/com/example/helloworld_tkachmyroslav/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.helloworld_tkachmyroslav;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void ChangeText(View view) {
+        TextView DemoLabel =findViewById(R.id.DemoLabel);
+        DemoLabel.setText("Hello! (Text was Changed)");
     }
 }
